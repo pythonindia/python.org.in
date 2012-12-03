e826bf8c70905d1efff00a557b6b2ffa043fdfcd
kracekumar <me@kracekumar.com>
2012-12-03 11:32:27 +0530

    Added blog page

---
+++ blog/content/coming-soon.rst
@@ -0,0 +1,9 @@
+coming-soon
+###########
+
+:date: 2012-12-03
+:slug: coming-soon
+:author: kracekumar
+:category: updates
+
+Currently blog is under development. 
---
+++ blog/pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Anand Chitipotu"
+SITENAME = u"python.org.in"
+SITEURL = 'http://python.org.in/blog'
+
+#TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          )
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          )
+
+THEME = 'theme'
+DEFAULT_PAGINATION = 10
